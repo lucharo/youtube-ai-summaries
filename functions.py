b0007a07a2a0f6d66ae9d369644d8a5e9d4a4ed4
Luis Chaves <lc5415@ic.ac.uk>
2023-04-06 17:05:57 +0100

    ok, long video takes a while

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import os
 import re
 import tempfile
 import pydub
+from pydub.silence import split_on_silence
 import openai
 import streamlit as st
 import yt_dlp as youtube_dl
@@ -12,13 +13,19 @@ def split_audio(audio_file, max_file_size):
     audio = pydub.AudioSegment.from_file(audio_file)
     duration = len(audio)
     max_duration = max_file_size * (duration / os.path.getsize(audio_file))
-    num_chunks = duration // max_duration + 1
-    return pydub.silence.split_on_silence(
-        audio, 
-        min_silence_len=max_duration, 
-        silence_thresh=-50,
-        keep_silence=100, 
-        num_chunks=num_chunks)
+    num_chunks = int(duration // max_duration + 1)
+
+    audio_chunks = split_on_silence(audio, min_silence_len=int(max_duration), silence_thresh=-50, keep_silence=100)
+    
+    # Ensure the number of chunks is not greater than num_chunks
+    print(0)
+    while len(audio_chunks) > num_chunks:
+        max_duration += max_duration // 10
+        audio_chunks = split_on_silence(audio, min_silence_len=int(max_duration), silence_thresh=-50, keep_silence=100)
+
+    print(2)
+
+    return audio_chunks
 
 def concatenate_transcripts(transcript_list):
     transcript = " ".join(transcript_list)
@@ -31,12 +38,21 @@ def transcribe_audio_file(audio_file):
     if file_size > MAX_FILE_SIZE:
         audio_chunks = split_audio(audio_file, MAX_FILE_SIZE)
         transcript_list = []
-        for chunk in audio_chunks:
+        progress_bar = st.progress(0)
+        num_chunks = len(audio_chunks)
+        for index, chunk in enumerate(audio_chunks):
+            print(index)
             with tempfile.NamedTemporaryFile(suffix=".webm") as temp_audio:
                 chunk.export(temp_audio.name, format="webm")
                 with open(temp_audio.name, "rb") as temp_audio_file:
-                    transcript_chunk = openai.Audio.transcribe("whisper-1", temp_audio_file)
+                    transcript_chunk = openai.Audio.transcribe(
+                        "whisper-1", 
+                        temp_audio_file
+                        )
                     transcript_list.append(transcript_chunk["text"])
+
+            progress_bar.progress((index + 1) / num_chunks)
+
         transcript = concatenate_transcripts(transcript_list)
     else:
         with open(audio_file, "rb") as audio:
@@ -77,7 +93,7 @@ def get_transcript(video_id):
             title = info["title"]
 
     # Perform transcription or subtitle extraction
-    with st.spinner('Transcribing audio'):
+    with st.spinner('Transcribing audio...'):
         with open(f"{video_id}.webm", "rb") as audio:
             transcript = transcribe_audio_file(f"{video_id}.webm")
 
--- ⚙️_App.py
+++ ⚙️_App.py
@@ -113,25 +113,8 @@ if __name__ == "__main__":
     if 'history' not in st.session_state:
         st.session_state.history = []
 
-    st.sidebar.title("Navigation")
-    st.sidebar.markdown(
-        """
-        **⚙️ App**: This is the main page where you can enter a YouTube URL, and the app will transcribe and summarize the video.
-
-        **📜 History**: This page displays a list of your previous transcriptions and summaries. You can revisit them without having to rerun the entire process.
-        """
-    )
-    st.sidebar.title("About")
-    st.sidebar.markdown(
-        """
-        This app has two main functionalities:
-
-        1. **Transcription**: It transcribes the audio from a given YouTube video using OpenAI's Whisper.
-        2. **Summarization**: It summarizes the transcribed text using OpenAI's GPT-3.5-turbo model.
-
-        To use the app, simply provide a valid YouTube URL and select your preferred summary language. You can also view the history of your previous requests.
-        """
-    )
+    with open('pages/sidebar.md', 'r') as sidebar_md:
+        st.sidebar.markdown(sidebar_md.read())
 
     login()
 
